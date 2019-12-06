59ccf78701d4e0f2e3c3051bf80fd75ff43186e5
Justin Chan <justin.the.c@gmail.com>
2019-12-06 18:55:22 -0500

    Added Neural Network Agent hooked up to the game. Currently random weights

--- main.js
+++ main.js
@@ -12,6 +12,18 @@ let Engine = Matter.Engine,
 const MAX_INPUT_MAGNITUDE = 0.002;
 const INPUT_MAGNITUDE = 0.002;
 const DASH_MULTIPLIER = 20;
+const UP_KEY = "KeyW";
+const LEFT_KEY = "KeyA";
+const DOWN_KEY = "KeyS";
+const RIGHT_KEY = "KeyD";
+const DASH_KEY = "Space";
+const INPUT_KEYS = [UP_KEY, LEFT_KEY, DOWN_KEY, RIGHT_KEY, DASH_KEY];
+const DIRECTION_VECTORS = {
+    [UP_KEY]: Vector.create(0, -INPUT_MAGNITUDE),
+    [LEFT_KEY]: Vector.create(-INPUT_MAGNITUDE, 0),
+    [DOWN_KEY]: Vector.create(0, INPUT_MAGNITUDE),
+    [RIGHT_KEY]: Vector.create(INPUT_MAGNITUDE, 0)
+};
 
 function capMagnitude(vec, maxMagnitude) {
     const magnitude = Vector.magnitude(vec);
@@ -66,15 +78,11 @@ function main() {
     // // run the renderer
     Render.run(render);
 
-    let up = Vector.create(0, -INPUT_MAGNITUDE);
-    let left = Vector.create(-INPUT_MAGNITUDE, 0);
-    let down = Vector.create(0, INPUT_MAGNITUDE);
-    let right = Vector.create(INPUT_MAGNITUDE, 0);
     // let upDownKeys = new Set(["KeyW", "KeyS"]);
     // let leftRightKeys = new Set(["KeyA", "KeyD"]);
     // let directionKeys = new Set([...upDownKeys, ...leftRightKeys]);
     let heldKeys = new Set([]);
-    let dashUp = true;
+    let dashAvailable = true;
 
     document.addEventListener("keydown", event => {
         console.log("keydown: "+event.code);
@@ -82,12 +90,16 @@ function main() {
     });
     document.addEventListener("keyup", event => {
         heldKeys.delete(event.code);
-        if (event.code === "Space") {
-            dashUp = true;
+        if (event.code === DASH_KEY) {
+            dashAvailable = true;
         }
     });
 
     let gameOver = false;
+
+
+    let nn = new NeuralNetwork(8, 10, 5);
+    let iter = 0;
     (function run() {
         if (!gameOver && !Bounds.contains(arena.bounds, player.position)) {
             gameOver = true;
@@ -98,24 +110,57 @@ function main() {
             alert("You Win!");
         }
 
+        if (!gameOver) {
+            // iter = (iter+1)%120;
+            if (iter === 0) {
+            
+                // Inputs
+                let px = player.position.x;
+                let py = player.position.y;
+                let pvx = player.velocity.x;
+                let pvy = player.velocity.y;
+                let ex = enemy.position.x;
+                let ey = enemy.position.y;
+                let evx = enemy.velocity.x;
+                let evy = enemy.velocity.y;
+                let input = [px, py, pvx, pvy, ex, ey, evx, evy];
+
+                // Output
+                let output = nn.feedForward(input);
+                // console.table(output.data[0]);
+
+                let outputKeys = output.data[0].map(x => Math.round(x));
+                let dashKeyHeld = outputKeys[4];
+
+                // console.table([wHeld, aHeld, sHeld, dHeld, spaceHeld]);
+                if (heldKeys.has(DASH_KEY) && !dashKeyHeld) { // Releasing dash key
+                    dashAvailable = true;
+                }
+                heldKeys.clear();
+                for (const [key_index, held] of outputKeys.entries()) {
+                    if (held)
+                        heldKeys.add(INPUT_KEYS[key_index]);
+                }
+                console.log(heldKeys);
+            }
+        }
+
 
         let inputForce = Vector.create(0,0);
-        if (heldKeys.has("KeyW"))
-            inputForce = Vector.add(inputForce, up);
-        if (heldKeys.has("KeyA"))
-            inputForce = Vector.add(inputForce, left);
-        if (heldKeys.has("KeyS"))
-            inputForce = Vector.add(inputForce, down);
-        if (heldKeys.has("KeyD"))
-            inputForce = Vector.add(inputForce, right);
+        for (let key of INPUT_KEYS) {
+            // console.log(key in DIRECTION_VECTORS.keys());
+            if (heldKeys.has(key) && (key in DIRECTION_VECTORS)) {
+                inputForce = Vector.add(inputForce, DIRECTION_VECTORS[key]);
+            }
+        }
         inputForce = capMagnitude(inputForce, MAX_INPUT_MAGNITUDE);
         Body.applyForce(player, player.position, inputForce);
 
-        if (heldKeys.has("Space") && dashUp) {
+        if (heldKeys.has(DASH_KEY) && dashAvailable) {
             let dashVector = Vector.mult(inputForce, DASH_MULTIPLIER);
             Body.applyForce(player, player.position, dashVector);
-            heldKeys.delete("Space");
-            dashUp = false;
+            heldKeys.delete(DASH_KEY);
+            dashAvailable = false;
         }
         window.requestAnimationFrame(run);
         Engine.update(engine, 1000 / 60);
